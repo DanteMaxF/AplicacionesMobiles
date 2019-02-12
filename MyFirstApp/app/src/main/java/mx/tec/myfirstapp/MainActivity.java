e12854f97723243a90d06993839f3ad2e841e728
DanteMaxF <A01207543@itesm.mx>
2019-02-11 19:08:15 -0600

    Pass parameters between activities

--- MyFirstApp/app/src/main/java/mx/tec/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/mx/tec/myfirstapp/MainActivity.java
@@ -1,26 +1,37 @@
 package mx.tec.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et1;
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNext = findViewById(R.id.btNext);
         et1 = findViewById(R.id.et1);
+
+        btnNext.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent nextIntent = new Intent(MainActivity.this, SecondActivity.class);
+                nextIntent.putExtra("username", et1.getText().toString());
+                startActivity( nextIntent );
+            }
+        });
     }
 
     //Next button method
-    public void Next(View view){
+    /*public void Next(View view){
         Intent nextIntent = new Intent(this, SecondActivity.class);
         nextIntent.putExtra("username", et1.getText().toString());
         startActivity( nextIntent );
-    }
+    }*/
 }
--- MyFirstApp/app/src/main/java/mx/tec/myfirstapp/SecondActivity.java
+++ MyFirstApp/app/src/main/java/mx/tec/myfirstapp/SecondActivity.java
@@ -4,26 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     private TextView tv1;
+    private Button btnPrev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+
+        btnPrev = findViewById(R.id.btPrev);
         tv1 = findViewById(R.id.tv1);
 
         String uname = getIntent().getStringExtra("username");
         tv1.setText("Hola "+ uname);
+
+        btnPrev.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent previous = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity( previous );
+            }
+        });
     }
 
     //Previous button method
+    /*
     public void Previous(View view){
         Intent previous = new Intent(this, MainActivity.class);
         startActivity( previous );
-    }
+    }*/
 }
